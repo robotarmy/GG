620d5b8de56fdd620ff7843950b037c3e0d86796
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2011-07-13 12:45:58 -0700

    sync

--- spec/requests/geo_photos_spec.rb
+++ spec/requests/geo_photos_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe "GeoPhotos" do
-  describe "GET /geo_photos" do
-    it "works! (now write some real specs)" do
-      get geo_photos_path
-      response.status.should be(200)
+  describe "GET /geo_photos/new" do
+    it "NEW GeoPhoto" do
+      visit new_geo_photo_path
+      page.attach_file('geo_photo[file]',geo_image_fixture_path)
+      page.click_on "SAVE"
     end
   end
 end
